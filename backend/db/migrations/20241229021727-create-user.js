61b0cfc7399a8c3d3a1f8e43118781234a9eb6a0
gabrdean <gabealexd@gmail.com>
2024-12-30 11:00:18 -0500

    All Pasing 3

--- backend/db/migrations/20241229021727-create-user.js
+++ backend/db/migrations/20241229021727-create-user.js
@@ -1,6 +1,5 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
@@ -51,7 +50,7 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    options.tableName = 'Users';
-    await queryInterface.dropTable(options);
+    options.tableName = "Users";
+    return queryInterface.dropTable(options);
   }
 };
